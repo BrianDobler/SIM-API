bd1a8db46cf60172b1ad151a969a48c5626a47a8
ginomassei <ginomassei@hotmail.com>
2021-09-26 17:44:28 -0300

    Controllers created for the distributions

--- controllers/normalDistribution.controller.js
+++ controllers/normalDistribution.controller.js
@@ -9,18 +9,19 @@ class NormalDistribution {
     generateValues () {
         let values = [];
         let x;
-
+    
         for (let index = 0; index < this.numberOfValues; index++) {
             let rnd1 = Math.random();
             let rnd2 = Math.random();
-
+            
             if (this.numberOfValues % 2 == 0) {
                 x = (Math.sqrt(-2 * Math.log(rnd1)) * Math.cos(2 * Math.PI * rnd2)) * this.sigma + this.mu;
             } else {
                 x = (Math.sqrt(-2 * Math.log(rnd1)) * Math.sin(2 * Math.PI * rnd2)) * this.sigma + this.mu;
             }
             x = (Math.round((x) * 10000.0) / 10000.0);
-            values.add(x);
+            
+            values[index] = x;
         }
         return values;
     }
--- controllers/uniformDistribution.controller.js
+++ controllers/uniformDistribution.controller.js
@@ -11,7 +11,7 @@ class UniformDistribution {
         for (let index = 0; index < this.numberOfValues; index++) {
             let x = this.lowerBound + (Math.random() * (this.upperBound - this.lowerBound));
             x = (Math.round((x) * 10000.0) / 10000.0);
-            values.add(x);
+            values[index] = x;
         }
         return values;
     }
