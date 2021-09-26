994965e8219da5622e8d2c98690a72abe0c32248
ginomassei <ginomassei@hotmail.com>
2021-09-26 18:59:27 -0300

    Refactored distribution routing

--- routes/distributions.routes.js
+++ routes/distributions.routes.js
@@ -1,3 +1,13 @@
 const distributionRouter = require('express').Router();
 
+const uniformController = require('../controllers/distributions/uniform.controller');
+const normalContoller = require('../controllers/distributions/normal.controller');
+const exponentialContoller = require('../controllers/distributions/exponential.controller');
+
+distributionRouter.post('/normal', normalContoller.generateValues);
+
+distributionRouter.post('/exponential', exponentialContoller.generateValues);
+
+distributionRouter.post('/uniform', uniformController.generateValues);
+
 module.exports = distributionRouter;
