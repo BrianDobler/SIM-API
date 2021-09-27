d2ca8db02535e266c09b404bb5442d4a5252a58e
ginomassei <ginomassei@hotmail.com>
2021-09-27 11:21:58 -0300

    Moved generator

---
+++ helpers/generators/LinearCongruentialGenerator.js
@@ -0,0 +1,20 @@
+function LinearCongruentialGenerator(
+    mod = 3578942135498,
+    a = 1234512354,
+    c = 1597563687,
+    seed = 489785354,
+) {
+    this.mod = mod;
+    this.a = a;
+    this.c = c;
+    this.seed = seed;
+
+    this.next = () => {
+        this.seed = ((this.a * this.seed + this.c) % this.mod);
+        return this.seed;
+    };
+
+    this.nextRandom = () => this.next() / this.mod;
+}
+
+module.exports = LinearCongruentialGenerator;
