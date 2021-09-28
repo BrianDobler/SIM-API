87ab5a68ed4146e22fd89da2bd36803089eac123
ginomassei <ginomassei@hotmail.com>
2021-09-28 10:51:02 -0300

    Montecarlo controller. Endpoint /montecarlo

---
+++ routes/montecarlo.routes.js
@@ -0,0 +1,5 @@
+const montecarloRouter = require('express').Router();
+
+const montecarloContoller = require('../controllers/montecarlo/montecarlo.controller');
+
+montecarloRouter.get('/simulate', montecarloContoller.simulate);
