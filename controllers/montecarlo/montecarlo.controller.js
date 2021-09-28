bcc540500cc5f04e17eacddefaeb097521e07be3
ginomassei <ginomassei@hotmail.com>
2021-09-28 11:09:20 -0300

    Mising module.exports

--- controllers/montecarlo/montecarlo.controller.js
+++ controllers/montecarlo/montecarlo.controller.js
@@ -1,4 +1,22 @@
-const montecarloContoller = {};
+const MontecarloSimulation = require('../../helpers/montecarlo/MontecarloSimulation');
+const Task = require('../../helpers/montecarlo/Task');
 
+const montecarloContoller = {};
 montecarloContoller.simulate = async (request, response) => {
+    const { body } = request;
+
+    // Create the tasks objects to pass to the montecarlo simulation.
+    const taskA1 = new Task();
+    const taskA2 = new Task();
+    const taskA3 = new Task();
+    const taskA4 = new Task();
+    const taskA5 = new Task();
+
+    // Create the montecarlo simulation.
+    const montecarlo = new MontecarloSimulation(taskA1, taskA2, taskA3, taskA4, taskA5);
+    montecarlo.setTaskPrecedeces();
+    montecarlo.setTaskDistributions();
+    montecarlo.setTaskDutrations();
 };
+
+module.exports = montecarloContoller;
--- routes/montecarlo.routes.js
+++ routes/montecarlo.routes.js
@@ -3,3 +3,5 @@ const montecarloRouter = require('express').Router();
 const montecarloContoller = require('../controllers/montecarlo/montecarlo.controller');
 
 montecarloRouter.get('/simulate', montecarloContoller.simulate);
+
+module.exports = montecarloRouter;
