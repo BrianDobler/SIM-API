d46a302c0b96aa5964cb9342afedc2bc50edb151
ginomassei <ginomassei@hotmail.com>
2021-09-30 21:18:36 -0300

    Adding the montecarlo simulation helper. Now working days and tasks precedences

--- helpers/montecarlo/MontecarloSimulation.js
+++ helpers/montecarlo/MontecarloSimulation.js
@@ -2,27 +2,17 @@ const UniformDistribution = require('../distributions/UniformDistribution');
 const ExponentialDistribution = require('../distributions/ExponentialDistribution');
 
 function MontecarloSimulation(taskA1, taskA2, taskA3, taskA4, taskA5) {
-    this.day = 0;
+    // Naive approach of the montecarlo simulation, to be reviewed.
     this.taskA1 = taskA1;
     this.taskA2 = taskA2;
     this.taskA4 = taskA3;
     this.taskA3 = taskA4;
     this.taskA5 = taskA5;
-
-    // State vector.
+    this.tasksRunning = false;
     this.stateVector = [];
 
-    this.setTaskPrecedeces = () => {
-        // Set task precedences according to the given default precedences.
-        this.taskA4.setPrecedence(this.taskA1);
-        this.taskA5.setPrecedence(this.taskA2);
-        this.taskA5.setPrecedence(this.taskA4);
-    };
-
-    this.nextDay = () => this.day++;
-
     this.setTaskDistributions = () => {
-        // Create objects for each distribution. For now all are fixed. There is not allowed to change.
+        // Create objects for each distribution. For now they're all are fixed.
         const taskA1Distribution = new UniformDistribution(20, 30);
         this.taskA1.setDistribution(taskA1Distribution);
 
@@ -39,8 +29,52 @@ function MontecarloSimulation(taskA1, taskA2, taskA3, taskA4, taskA5) {
         this.taskA5.setDistribution(taskA5Distribution);
     };
 
-    this.calculateTaskDurations = () => {
+    this.simulate = (numberOfDays) => {
+        for (let day = 1; day <= numberOfDays; day++) { // Counter starts from 1, 'cause needed to pass rows to front.
+            // If there is no tasks running then start new tasks.
+            if (!this.tasksRunning) {
+                // Calulate the elapsed times of the tasks that have no precedences.
+                this.taskA1.calculateTimeToComplete();
+                this.taskA2.calculateTimeToComplete();
+                this.taskA3.calculateTimeToComplete();
 
+                this.tasksRunning = true; // Set the flag, there are tasks running now.
+            }
+
+            if (this.taskA1.completed && this.taskA2.completed && this.taskA3.completed && this.taskA4.completed && this.taskA5.completed) {
+                // If all tasks completed. Then set flags to false.
+                this.tasksRunning = false;
+                this.taskA1.completed = false;
+                this.taskA2.completed = false;
+                this.taskA3.completed = false;
+                this.taskA4.completed = false;
+                this.taskA5.completed = false;
+            }
+
+            if (this.taskA1.completed && this.taskA4.timeToCompleted == null && !this.taskA4.completed) {
+                // If task a1 is completed and task4 is not started. Then start task A4.
+                this.taskA4.calculateTimeToComplete();
+            }
+
+            if (this.taskA2.completed && this.taskA4.completed && !this.taskA5.completed && this.taskA5.timeToCompleted == null) {
+                // If task A2 and A4 are completed, then start A5.
+                this.taskA5.calculateTimeToComplete();
+            }
+
+            console.log(
+                `TASK A1: ${this.taskA1.timeToCompleted} | `
+                + `TASK A2: ${this.taskA2.timeToCompleted} | `
+                + `TASK A3: ${this.taskA3.timeToCompleted} | `
+                + `TASK A4: ${this.taskA4.timeToCompleted} | `
+                + `TASK A5: ${this.taskA5.timeToCompleted}`,
+            );
+            // A day less of each task.
+            this.taskA1.aDayLess();
+            this.taskA2.aDayLess();
+            this.taskA3.aDayLess();
+            this.taskA4.aDayLess();
+            this.taskA5.aDayLess();
+        }
     };
 }
 
--- helpers/montecarlo/Task.js
+++ helpers/montecarlo/Task.js
@@ -1,24 +1,32 @@
 function Task() {
-    this.precedence = [];
     this.distribution = null;
     this.completed = false;
-    this.taskDuration = null;
-
-    this.setPrecedence = (task) => {
-        // Set task precedence.
-        this.precedence.push(task);
-    };
+    this.timeToCompleted = null;
 
     this.setDistribution = (distribution) => {
         // Set the distribution of the task.
         this.distribution = distribution;
     };
 
-    this.calculateTaskDuration = () => {
+    this.calculateTimeToComplete = () => {
         // Calculate the tsak duration based on the probability.
         const randomValue1 = Math.random();
         const randomValue2 = Math.random();
-        this.taskDuration = this.distribution.nextValue(randomValue1, randomValue2);
+        this.timeToCompleted = Math.floor(this.distribution.nextValue(randomValue1, randomValue2));
+        this.completed = false;
+    };
+
+    this.aDayLess = () => {
+        // Decrement a day of time to complete a task.
+        if (this.timeToCompleted > 0) {
+            this.timeToCompleted--;
+        } else if (this.timeToCompleted == null) {
+            return null; // If there is no days to decrement simply do nothing.
+        } else { // If no days. Completed TRUE, Time to completed reset.
+            this.timeToCompleted = null;
+            this.completed = true;
+        }
+        return true; // Not sure how to perform this. But now it's working.
     };
 }
 
