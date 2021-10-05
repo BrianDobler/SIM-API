0acc95b98ee006f0099f998d70825c8b294efa65
ginomassei <ginomassei@hotmail.com>
2021-10-05 11:02:41 -0300

    Ahora, puedo elegir la distribución generadora y el generador de números aleatorios. Todo dinámicamente dependiendo de como se instancie el objeto.

--- helpers/montecarlo/Task.js
+++ helpers/montecarlo/Task.js
@@ -1,17 +1,38 @@
+const UniformDistribution = require('../distributions/UniformDistribution');
+const NormalDistribution = require('../distributions/NormalDistribution');
+const ExponentialDistribution = require('../distributions/ExponentialDistribution');
+const LCG = require('../generators/LinearCongruentialGenerator');
+
 function Task() {
     this.distribution = null;
     this.completed = false;
     this.timeToCompleted = null;
+    this.generator = null;
 
-    this.setDistribution = (distribution) => {
+    this.setDistribution = (distributionType, parameters) => {
         // Set the distribution of the task.
-        this.distribution = distribution;
+        if (distributionType === 'uniform') {
+            const taskDistribution = new UniformDistribution(parameters.A, parameters.B);
+            this.distribution = taskDistribution;
+        } else if (distributionType === 'normal') {
+            const taskDistribution = new NormalDistribution(parameters.mu, parameters.sigma);
+            this.distribution = taskDistribution;
+        } else if (distributionType === 'exponential') {
+            const taskDistribution = new ExponentialDistribution(parameters.lambda);
+            this.distribution = taskDistribution;
+        }
+    };
+
+    this.setGenerator = (generator) => {
+        // Set the random number generator for the task.
+        this.generator = (generator == null) ? Math : new LCG(generator.mod, generator.a, generator.c, generator.seed);
     };
 
     this.calculateTimeToComplete = () => {
         // Calculate the tsak duration based on the probability.
-        const randomValue1 = Math.random();
-        const randomValue2 = Math.random();
+        const randomValue1 = this.generator.random();
+        const randomValue2 = this.generator.random();
+
         this.timeToCompleted = Math.floor(this.distribution.nextValue(randomValue1, randomValue2));
         this.completed = false;
     };
