872bcc817c1b04a278b5f9fcbe86a8648a2a3ff5
ginomassei <ginomassei@hotmail.com>
2021-10-12 00:07:26 -0300

    Variance and SD working

--- helpers/montecarlo/MontecarloSimulation.ts
+++ helpers/montecarlo/MontecarloSimulation.ts
@@ -1,20 +1,23 @@
 import Task from './Task';
 
-export default class MontecarloSimulation {
+export class MontecarloSimulation {
     // Naive approach of the montecarlo simulation, to be reviewed.
     taskA1: Task;
     taskA2: Task;
     taskA3: Task;
     taskA4: Task;
     taskA5: Task;
-    path4: number;
-    path5: number;
-    assemblyTaskDuration: number;
-    assemblyTask45Counter: number;
-    simulation: number;
-    mean: number;
-    min: number;
-    max: number;
+    path4: number = 0;
+    path5: number = 0;
+    assemblyTaskDuration: number = 0;
+    assemblyTask45Counter: number = 0;
+    simulation: number = 0;
+    mean: number = 0;
+    min: number = 0;
+    max: number = 0;
+    variance: number = 0;
+    standardDeviation: number = 0;
+    fechaNC90: number = 0;
 
     constructor(taskA1: Task, taskA2: Task, taskA3: Task, taskA4: Task, taskA5: Task) {
         this.taskA1 = taskA1;
@@ -22,15 +25,6 @@ export default class MontecarloSimulation {
         this.taskA3 = taskA3;
         this.taskA4 = taskA4;
         this.taskA5 = taskA5;
-
-        this.path4 = 0;
-        this.path5 = 0;
-        this.assemblyTaskDuration = 0;
-        this.assemblyTask45Counter = 0;
-        this.simulation = 0;
-        this.mean = 0;
-        this.min = 0;
-        this.max = 0;
     }
 
     simulate = (): void => {
@@ -58,15 +52,29 @@ export default class MontecarloSimulation {
         // Set the assembly task duration. Based on the longest path.
         this.assemblyTaskDuration = (this.path4 >= this.path5) ? this.path4 : this.path5;
 
+        // Update from the last row the parameters.
         this.getMean();
+        this.getVariance();
+        this.getStandardDeviation();
         this.updateBounds();
     }
 
     getMean = (): void => {
         this.mean = Math.round((((this.mean * (this.simulation - 1)) + this.assemblyTaskDuration) / this.simulation) * 100.0) / 100.0;
+        // this.mean = ((this.mean * (this.simulation - 1)) + this.assemblyTaskDuration) / this.simulation;
     }
 
     getProbability = (): number => (Math.round((this.assemblyTask45Counter / this.simulation) * 10000.0) / 10000.0);
+
+    getVariance = (): void => {
+        const variance = (((this.simulation - 2) * this.variance) + ((this.simulation / (this.simulation - 1)) * ((this.mean - this.assemblyTaskDuration) ** 2))) / (this.simulation - 1);
+        this.variance = (this.simulation === 1) ? 0 : Math.round((variance) * 10000.0) / 10000.0;
+    }
+
+    getStandardDeviation = (): void => {
+        const standardDeviation = Math.sqrt(this.variance);
+        this.standardDeviation = Math.round((standardDeviation) * 10000.0) / 10000.0;
+    }
 
     updateBounds = (): void => {
         // Checks for the min or max assembly task duration and stores it.
@@ -97,5 +105,7 @@ export default class MontecarloSimulation {
         assemblyTaskDuration: this.assemblyTaskDuration,
         day: this.simulation,
         mean: this.mean,
+        variance: this.variance,
+        standardDeviation: this.standardDeviation,
     });
 }
